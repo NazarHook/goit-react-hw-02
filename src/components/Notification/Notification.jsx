f6b53b553586e367d293a80b74cf9213abbd7968
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-04-07 02:27:12 +0300

    fixed some syntax

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,7 @@ import css from './Notification.module.css'
 export default function Notification({percentage, result}) {
    return (
     <div>
-        {result === 0 ? <p className={css.message}>No feedback yet</p> : (<div className={css.feedStats}><p>Total: {result} </p><p>Positive: {percentage}</p></div>)}
+        {result === 0 ? <p className={css.message}>No feedback yet</p> : (<div className={css.feedStats}><p>Total: {result} </p><p>Positive: {percentage}%</p></div>)}
         </div>
 )
 }
